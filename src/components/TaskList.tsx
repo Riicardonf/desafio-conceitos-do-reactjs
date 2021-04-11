13872fff6aea734c03767d6c4f4c6ab62832970c
Ricardo Nascimento <ricardobatistanf@gmail.com>
2021-04-11 02:35:46 -0300

    Completing test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle) { return;  }
+
+    const task: Task = {
+      id:Math.random(),
+      title: newTaskTitle,
+      isComplete:false
+    }
+
+    setTasks([...tasks, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const toogleTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(toogleTask);
+
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  function handleRemoveTask(id: number) { 
+    const newArray = tasks.filter(task => task.id !== id);
+
+    setTasks(newArray)
   }
 
   return (
